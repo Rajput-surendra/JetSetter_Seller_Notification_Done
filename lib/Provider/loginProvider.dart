ad4f78935448941d1e34cb617b1ce02fc4fcce03
Rajput-surendra <surendraalphawizz@gmail.com>
2023-11-04 18:29:12 +0530

    first commit

---
+++ lib/Provider/loginProvider.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '../Helper/ApiBaseHelper.dart';
+import '../Screen/Authentication/Login.dart';
+import '../Screen/DeshBord/dashboard.dart';
+import '../Widget/api.dart';
+import '../Widget/parameterString.dart';
+import '../Widget/sharedPreferances.dart';
+
+class LoginProvider extends ChangeNotifier {
+  String? mobile, password, username, id,fId;
+  AnimationController? buttonController;
+
+  get getMobilenumber => mobile;
+  get getPassword => Password;
+
+  Future<void> getLoginUser(
+
+    BuildContext context,
+    GlobalKey<ScaffoldMessengerState> scaffoldMessengerKey,
+    Function updateNow,
+      String? fId
+  ) async {
+    var data = {
+      Mobile: mobile,
+      Password: password,
+      'fcm_id':fId
+    };
+    print('___this is is paramersts sf6 _______${data}_________');
+
+    ApiBaseHelper().postAPICall(getUserLoginApi, data).then(
+      (getdata) async {
+        bool error = getdata["error"];
+        String? msg = getdata["message"];
+        if (!error) {
+          setSnackbarScafold(scaffoldMessengerKey, context, msg!);
+          var data = getdata["data"][0];
+          id = data[Id];
+          username = data[Username];
+          mobile = data[Mobile];
+          saveUserDetail(
+            id!,
+            username!,
+            mobile!,
+          );
+          setPrefrenceBool(isLogin, true);
+          Navigator.pushReplacement(
+            context,
+            CupertinoPageRoute(
+              builder: (context) =>  const Dashboard(),
+            ),
+          );
+        } else {
+          await buttonController!.reverse();
+          setSnackbarScafold(scaffoldMessengerKey, context, msg!);
+          updateNow();
+        }
+      },
+      onError: (error) {
+        setSnackbarScafold(scaffoldMessengerKey, context, error.toString());
+      },
+    );
+  }
+}
